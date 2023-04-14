97d333ad2320cd8aa3204cc4da8be1e012e71884
= <MaxsoftLtd>
2023-04-14 14:01:30 +0300

    New cors

--- app.js
+++ app.js
@@ -27,6 +27,18 @@ app.use(cors({ credentials: true, origin: [URL, URL_CLIENT] }));
 // 	res.send("Api is running...");
 // });
 
+app.all("*", function (req, res, next) {
+	const origin = cors.origin.includes(req.header("origin").toLowerCase())
+		? req.headers.origin
+		: cors.default;
+	res.header("Access-Control-Allow-Origin", origin);
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	next();
+});
+
 mongoose
 	.connect(URI)
 	.then(() => console.log("Connected to DataBase"))
